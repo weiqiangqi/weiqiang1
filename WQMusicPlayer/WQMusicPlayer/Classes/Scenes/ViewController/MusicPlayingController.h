fd2919833b2f3766898a6788102882038e20e32d
weiqiang <qiqweiqiang10@163.com>
2015-09-15 12:59:04 +0800

    no message

---
+++ WQMusicPlayer/WQMusicPlayer/Classes/Scenes/ViewController/MusicPlayingController.h
@@ -0,0 +1,13 @@
+//
+//  MusicPlayingController.h
+//  WQMusicPlayer
+//
+//  Created by lanou3g on 15/9/15.
+//  Copyright (c) 2015年 齐伟强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MusicPlayingController : UIViewController
+
+@end
---
+++ WQMusicPlayer/WQMusicPlayer/Classes/Scenes/ViewController/MusicPlayingController.m
@@ -0,0 +1,37 @@
+//
+//  MusicPlayingController.m
+//  WQMusicPlayer
+//
+//  Created by lanou3g on 15/9/15.
+//  Copyright (c) 2015年 齐伟强. All rights reserved.
+//
+
+#import "MusicPlayingController.h"
+
+@interface MusicPlayingController ()
+
+@end
+
+@implementation MusicPlayingController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
