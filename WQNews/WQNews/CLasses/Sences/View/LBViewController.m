244d49dc2ceebcb471eaa726d241b4f2b6fd90c3
weiqiang <qiqweiqiang10@163.com>
2015-09-23 14:07:28 +0800

    [5]完善了一下轮播详情

--- WQNews/WQNews/CLasses/Sences/View/LBViewController.m
+++ WQNews/WQNews/CLasses/Sences/View/LBViewController.m
@@ -10,6 +10,11 @@
 #import "IanScrollView.h"
 
 @interface LBViewController ()
+//存储对应的一组照片
+@property(nonatomic,strong)NSMutableArray * mutArray;
+//当前的视图
+@property(nonatomic,assign)NSInteger cureentIndex;
+
 
 @end
 
@@ -17,11 +22,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor greenColor];
+    self.cureentIndex = 0;
+    self.view.backgroundColor = [UIColor colorWithRed:0.797 green:1.000 blue:0.311 alpha:1.000];
+    
     [self drawLBScrollView];
-    
-    
+    [self drawView];
+    [self drawFootSection];
 }
+
 
 
 
@@ -39,26 +47,67 @@
     NSMutableArray * PICmutArray = [[NSMutableArray alloc]initWithCapacity:8];
      NSDictionary * dict = self.LBnews.pics;
     NSArray * PICarray = dict[@"list"];
-    NSLog(@"%@",PICarray);
+    self.mutArray = [PICarray mutableCopy];
+    NSLog(@"%@",self.mutArray);
     for (NSDictionary * Dict in PICarray) {
         NSString * URL = Dict[@"kpic"];
         [PICmutArray addObject:URL];
     }
-    NSLog(@"%@",PICmutArray);
     scrollView.slideImagesArray = PICmutArray;
+    scrollView.ianCurrentIndex = ^(NSInteger index){
+        self.cureentIndex = index;
+        //刷新图片注释
+        [self drawFootSection];
+    };
+    
     [scrollView startLoading];
     [self.view addSubview:scrollView];
 }
-
+//绘制表头
 - (void)drawView
 {
+    UIView * titleView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, kScreenWidth, 64)];
+    titleView.alpha = 0.5;
+    titleView.backgroundColor = [UIColor orangeColor];
+    UIButton * backButton = [[UIButton alloc]initWithFrame:CGRectMake(0, 30, 40, 30)];
+    [backButton setImage:[UIImage imageNamed:@"back"] forState:UIControlStateNormal];
+    
+    [backButton addTarget:self action:@selector(backToToutiao) forControlEvents:UIControlEventTouchUpInside];
+    
+    [titleView addSubview:backButton];
+    [self.view addSubview:titleView];
+}
+
+- (void)drawFootSection
+{
+    UIView * FootView = [[UIView alloc]initWithFrame:CGRectMake(0, kScreenHeight - 100, kScreenWidth, 100)];
+    FootView.backgroundColor = [UIColor orangeColor];
     
     
+    
+    UILabel * badyLable = [[UILabel alloc]initWithFrame:CGRectMake(10, 30, kScreenWidth - 20, 40)];
+    NSDictionary * dict = self.mutArray[self.cureentIndex];
+    badyLable.text = dict[@"alt"];
+    badyLable.numberOfLines = 0 ;
+    badyLable.font = [UIFont systemFontOfSize:13];
+    [FootView addSubview:badyLable];
+    [self.view addSubview:FootView];
     
 }
 
+#pragma mark ---内部写的方法--
+- (void)backToToutiao
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
-
+#pragma mark --lazy load--
+-(NSMutableArray *)mutArray{
+    if (_mutArray == nil) {
+        _mutArray = [[NSMutableArray alloc]initWithCapacity:8];
+    }
+    return _mutArray;
+}
 
 /*
 #pragma mark - Navigation
